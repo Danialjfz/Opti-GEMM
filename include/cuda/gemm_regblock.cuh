f20fc764ee8b695c8ec11e97e9e121dcf9b22cf2
Danial Jafarzadeh <danialj999@gmail.com>
2026-06-16 16:02:25 +0330

    nothing

--- include/cuda/gemm_regblock.cuh
+++ include/cuda/gemm_regblock.cuh
@@ -1,4 +1,3 @@
 #pragma once
 
-// Register‑blocked GEMM for high arithmetic intensity
-void gemm_regblock(const float* A, const float* B, float* C, int M, int N, int K);
+void regblock_gemm(const float* A, const float* B, float* C, int M, int N, int K);
