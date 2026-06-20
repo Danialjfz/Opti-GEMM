12434a3bd4bb2fbcef2a5763c50f3f78d139e386
Danial Jafarzadeh <danialj999@gmail.com>
2026-06-20 19:05:21 +0330

    small adjustments to the gemm tiled implementation and the correctness test

--- include/cuda/gemm_tiled.cuh
+++ include/cuda/gemm_tiled.cuh
@@ -1,7 +1,5 @@
 #pragma once
-
-__global__ void tiled_gemm(
-    const float* __restrict__ A,
-    const float* __restrict__ B,
-    float*       __restrict__ C,
-    int M, int N, int K);
+__global__ void tiled_gemm(const float* __restrict__ A,
+                           const float* __restrict__ B,
+                           float*       __restrict__ C,
+                           int M, int N, int K);
